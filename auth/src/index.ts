9f32dc3ce011375f3bfd1ef02133f8c23d867044
Ismile Hossain <iamismile.dev@gmail.com>
2023-09-14 00:34:21 +0600

    skaffold auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+// Config
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+// Routes
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
