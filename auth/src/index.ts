c7b6332f6cd4558f3e6fdf2ece50d0cebeb41108
Ismile Hossain <iamismile.dev@gmail.com>
2023-09-18 00:10:59 +0600

    error handling & response normalization

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import 'express-async-errors';
 
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -14,6 +17,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
