f71c7ad1866dfec1a418f0ab78257653a629cc95
Ismile Hossain <iamismile.dev@gmail.com>
2023-10-09 01:07:46 +0600

    testing isolated microservies

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,6 @@
-import cookieSession from 'cookie-session';
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
 
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middlewares/error-handler';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-// Config
-const app = express();
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-// Routes
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from './app';
 
 const bootstrap = async () => {
   if (!process.env.JWT_KEY) {
